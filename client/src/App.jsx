a634a9f4414bb33bd5584f622e4e6d4a39c317ac
kunalrana9458 <kunalranabsr9458@gmail.com>
2024-05-09 14:27:04 +0530

    Stated our Mern Blog Project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-gray-500'>First Mern Project</h1>
+  )
+}
+
+export default App

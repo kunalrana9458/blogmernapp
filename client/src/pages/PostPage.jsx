6dcfe3432196497c87c05d5f3f4a1881ae2f578f
kunalrana9458 <kunalranabsr9458@gmail.com>
2024-05-29 13:35:12 +0530

    recent article section is added in post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,69 +1,117 @@
-import { Button, Spinner } from 'flowbite-react';
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import CallToAction from '../components/CallToAction';
-import CommentSection from '../components/CommentSection';
-
+import { Button, Spinner } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
-  const {postSlug} = useParams();
-  const [loading,setLoading] = useState(false);
-  const [error,setError] = useState(false);
-  const [post,setPost] = useState(null);
+  const { postSlug } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [post, setPost] = useState(null);
+  const [recentPost, setRecentPost] = useState(null);
 
   useEffect(() => {
-    const fetchPost = async() => {
-        try {
-            setLoading(true);
-            const res = await fetch(`/api/post/getposts/?slug=${postSlug}`);
-            const data = await res.json();
-            if(!res.ok){
-                setError(true);
-                setLoading(false);
-                return ;
-            }
-            if(res.ok){
-                setPost(data.post[0]);
-                setLoading(false);
-                setError(false)
-            }
-        } catch (error) {
-            setLoading(false);
-            setError(true);   
+    const fetchPost = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/post/getposts/?slug=${postSlug}`);
+        const data = await res.json();
+        if (!res.ok) {
+          setError(true);
+          setLoading(false);
+          return;
         }
+        if (res.ok) {
+          setPost(data.post[0]);
+          setLoading(false);
+          setError(false);
+        }
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
     };
     fetchPost();
-},[postSlug])
+  }, [postSlug]);
 
-  if(loading) return (
-    <div className='min-h-screen flex justify-center items-center'>
-        <Spinner size='xl' />
-    </div>
-  )
+
+  useEffect(() => {
+    try {
+      const fetchRecentPost = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if(res.ok){
+          setRecentPost(data.post);
+        }
+      }
+      fetchRecentPost();
+    } catch (error) {
+      
+    }
+  })
+
+  if (loading)
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Spinner size="xl" />
+      </div>
+    );
 
   return (
-    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-       <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl' > {post && post.title} </h1>
-       <Link to={`/search?category=${post && post.category}`}  
-       className='self-center mt-5'>
-        <Button color='gray' pill size='xs'> {post && post.category} </Button>
-       </Link>
-       <img src={post && post.image} alt={post && post.title}
-       className='mt-10 p-3 max-h-[600px] w-full object-cover' />
-       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto
-       w-full max-w-2xl text-xs '>
-         <span> {post && new Date(post.createdAt).toLocaleDateString()} </span>
-         <span className='italic'> {post && (post.content.length/1000).toFixed(0) } mins read </span>
-       </div>
-       <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html:post && post.content}}>
-       </div>
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {" "}
+        {post && post.title}{" "}
+      </h1>
+      <Link
+        to={`/search?category=${post && post.category}`}
+        className="self-center mt-5"
+      >
+        <Button color="gray" pill size="xs">
+          {" "}
+          {post && post.category}{" "}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
+      <div
+        className="flex justify-between p-3 border-b border-slate-500 mx-auto
+       w-full max-w-2xl text-xs "
+      >
+        <span> {post && new Date(post.createdAt).toLocaleDateString()} </span>
+        <span className="italic">
+          {" "}
+          {post && (post.content.length / 1000).toFixed(0)} mins read{" "}
+        </span>
+      </div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
 
-       <div className='max-w-4xl mx-auto w-full'>
-         <CallToAction />
-       </div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
 
-       <CommentSection postId={post && post._id} />
+      <CommentSection postId={post && post._id} />
 
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent Articles</h1>
+        <div className="flex lg:flex-wrap gap-4 mt-5
+        justify-center">
+          {
+            recentPost && 
+            recentPost.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))
+          }
+        </div>
+      </div>
     </main>
-  )
+  );
 }

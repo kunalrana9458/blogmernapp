c0101e497895c11943402e050d6426fbdfc70da0
kunalrana9458 <kunalranabsr9458@gmail.com>
2024-05-31 17:44:25 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      
+    <div className='min-h-screen flex items-center justify-center'>
+        <div className="max-w-2xl mx-auto p-3 text-center">
+      <div >
+          <h1 className='text-3xl font font-semibold text-center my-7'>About KR's Blog</h1>
+          <div className="text=md text-gray-500 flex flex-col gap-6">
+            <p> 
+            Welcome to KR's Blog We're delighted to have you here. Our platform is designed to empower creators and readers alike, providing a space where individuals can share their thoughts, stories, and expertise with the world.
+            </p>
+            <p>
+            KR's serves as a platform for expression, allowing users to unleash their creativity and share their passions with the world. Whether you're writing about technology, travel, food, fashion, or anything in between, our platform provides the tools and support you need to bring your ideas to life.
+            </p>
+            <p>
+            Discover a wealth of engaging content on KR's From thought-provoking essays to practical tutorials, our platform offers a diverse range of topics to explore. Whether you're seeking inspiration, entertainment, or knowledge, you'll find it here.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
